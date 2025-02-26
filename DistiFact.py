b21387290de95a77ed007f3c9ef735f88a89ffcf
Jose M. Caballero <josemcaba@gmail.com>
2025-02-27 00:55:04 +0100

    Importacion de extractores segun empresa

--- DistiFact.py
+++ DistiFact.py
@@ -4,78 +4,7 @@ import pandas as pd
 import os
 import funciones_comunes as ft
 from mostrar_menu import seleccionar_proveedor, seleccionar_tipo_PDF
-
-def extraer_numero_factura(seccion):
-    """
-    Extrae el número de factura de una sección de texto.
-    Retorna el número como entero o 0 si no se encuentra.
-    """
-    num_factura = re.search(r"Nº factura\s*(\d+)", seccion)
-    return int(num_factura.group(1)) if num_factura else 0
-
-def extraer_fecha(seccion):
-    """
-    Extrae la fecha de emisión de una sección de texto.
-    Retorna la fecha como cadena o 0 si no se encuentra.
-    """
-    fecha = re.search(r"Fecha emisión\s*(.*)", seccion)
-    if not fecha:
-        return 0
-    fecha = re.sub(r"[.,]", "", fecha.group(1))
-    return fecha
-
-def extraer_base_iva(seccion):
-    """
-    Extrae la base imponible del IVA de una sección de texto.
-    Retorna el valor como float o 0.0 si no se encuentra.
-    """
-    base = re.search(r"Base\s*([\d,\.]+)", seccion)
-    return ft.convertir_a_float(base.group(1)) if base else 0.0
-
-def extraer_cuota_iva(seccion):
-    """
-    Extrae la cuota de IVA de una sección de texto.
-    Retorna el valor como float o 0.0 si no se encuentra.
-    """
-    iva = re.search(r"IVA\s*([\d,\.]+)", seccion)
-    return ft.convertir_a_float(iva.group(1)) if iva else 0.0
-
-def extraer_total_factura(seccion):
-    """
-    Extrae el total de la factura de una sección de texto.
-    Retorna el valor como flotante o 0.0 si no se encuentra.
-    """
-    total_match = re.search(r"Total\s*([\d,\.]+)\s*€?", seccion)
-    return ft.convertir_a_float(total_match.group(1)) if total_match else 0.0
-
-def extraer_nombre_cliente(seccion, nombre_proveedor):
-    """
-    Extrae el nombre del cliente a partir del patrón "Pescadería Salvador".
-    Se asume que en la sección aparece una línea con el formato:
-    "Pescadería Salvador [Nombre del cliente]"
-
-    Retorna el nombre del cliente (cadena) o 0 si no se encuentra.
-    """
-    nombre = re.search(rf"{nombre_proveedor}\s*(.+)", seccion)
-    return nombre.group(1).strip() if nombre else 0
-
-def extraer_nif_cliente(seccion, nif_proveedor):
-    """
-    Extrae el NIF del cliente a partir del patrón "25041071-M".
-    Se asume que en la sección aparece una línea con el formato:
-    "25041071-M [NIF del cliente]"
-
-    Retorna el NIF validado (cadena) si se encuentra y es válido, 
-    "NIF Inválido" si la validación falla o 0 si no se encuentra.
-    """
-    nif = re.search(rf"{nif_proveedor}\s*(.+)", seccion)
- 
-    if nif:
-        nif = re.sub(r"[^a-zA-Z0-9]", "", nif.group(1)).upper()
-        if nif == 'X3581661W':
-            nif = 'X3586116W'
-        return nif if ft.validar_nif(nif) else "NIF Inválido"
-    return 0
+import extractorPescaderia as extraer
 
 def procesar_seccion(seccion, nombre_proveedor, nif_proveedor):
     """
@@ -86,22 +15,22 @@ def procesar_seccion(seccion, nombre_proveedor, nif_proveedor):
         return None
 
     datos_factura = {
-        "Num. Factura": extraer_numero_factura(seccion),
-        "Fecha Fact.": extraer_fecha(seccion),
-        "Fecha Oper.": extraer_fecha(seccion),
+        "Num. Factura": extraer.numero_factura(seccion),
+        "Fecha Fact.": extraer.fecha(seccion),
+        "Fecha Oper.": extraer.fecha(seccion),
         "Concepto": 700,
-        "Base I.V.A.": extraer_base_iva(seccion),
+        "Base I.V.A.": extraer.base_iva(seccion),
         "% I.V.A.": 0,
-        "Cuota I.V.A.": extraer_cuota_iva(seccion),
-        "Base I.R.P.F.": extraer_base_iva(seccion),
+        "Cuota I.V.A.": extraer.cuota_iva(seccion),
+        "Base I.R.P.F.": extraer.base_iva(seccion),
         "% I.R.P.F.": 0,
         "Cuota I.R.P.F.": 0,
-        "Base R. Equiv.": extraer_base_iva(seccion),
+        "Base R. Equiv.": extraer.base_iva(seccion),
         "% R. Equiv.": 0,
         "Cuota R. Equiv.": 0,
-        "NIF/DNI": extraer_nif_cliente(seccion, nif_proveedor),
-        "Nombre": extraer_nombre_cliente(seccion, nombre_proveedor),
-        "Total Factura": extraer_total_factura(seccion)
+        "NIF/DNI": extraer.nif_cliente(seccion, nif_proveedor),
+        "Nombre": extraer.nombre_cliente(seccion, nombre_proveedor),
+        "Total Factura": extraer.total_factura(seccion)
     }
 
     # Calcular % I.V.A.
